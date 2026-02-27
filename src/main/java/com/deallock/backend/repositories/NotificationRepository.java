b9531e0febc1485ba6aea8e6c99fa59e8e00fd5a
Gideon Ibitoye <ibitoyrgideon79@gmail.com>
2026-02-27 12:56:06 +0100

    Fix notification repository methods

--- src/main/java/com/deallock/backend/repositories/NotificationRepository.java
+++ src/main/java/com/deallock/backend/repositories/NotificationRepository.java
@@ -4,8 +4,15 @@ import com.deallock.backend.entities.Notification;
 import com.deallock.backend.entities.User;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
     List<Notification> findByUserOrderByCreatedAtDesc(User user);
-    long countByUserAndReadFalse(User user);
+    long countByUserAndReadIsFalse(User user);
+
+    @Modifying
+    @Query("update Notification n set n.read = true where n.user = :user and n.read = false")
+    int markAllReadByUser(@Param("user") User user);
 }
